1b7faa49ded9b9c9d1dfa37d018e51d6a7c1868d
Glenn <gho7004230@sbcglobal.net>
2021-08-31 20:55:43 -0500

    Create install_powershell.sh

---
+++ install_powershell.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Register the Microsoft RedHat repository
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | tee /etc/yum.repos.d/microsoft.repo
+
+# Install PowerShell
+yum install -y powershell
+
+# Start PowerShell
+pwsh
